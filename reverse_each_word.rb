db735ca6c2517417bab205712f069ad3797c7d0f
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-06 05:42:01 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+def reverse_each_word(sentence)
+
+         #sentence.split()
+         #str=sentence.split()
+         #strnew=[]
+         #str.map do |i|
+          #   strnew.push(i.reverse)
+         #end
+         #strnew.join(" ")
+
+         sentence.split()
+         str=sentence.split()
+         str.collect do |i|
+             i.reverse
+         end.join(" ")
+
+
+
+end
